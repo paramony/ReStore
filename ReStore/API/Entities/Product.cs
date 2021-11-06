937670c51f498dbf0b8d063b2fbb3aeb7d0c8d1e
mohsen khorasani <pes202020@hotmail.com>
2021-11-06 21:13:31 +0330

    End Of Section 2

---
+++ ReStore/API/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public long Price { get; set; }
+        public string Description { get; set; } = default!;
+        public string PictureUrl { get; set; } = default!;
+        public string Type { get; set; } = default!;
+        public string Brand { get; set; } = default!;
+        public int QuantityInStock { get; set; }
+    }
+}
